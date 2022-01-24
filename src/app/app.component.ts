b57894c582718461275e4649aa22541ef01793b7
YS_workPC <ong_ys86@yahoo.com.sg>
2022-01-24 14:46:37 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'My1stAngular';
+  title = 'My 1st Angular';
+
+  sayHello(){
+    alert("Hello!");
+  }
+
 }
